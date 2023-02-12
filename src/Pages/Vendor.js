db477f718113615ee51f055669afbe198f263461
Justin Klaver <Klaverts@yahoo.com>
2023-02-11 22:23:16 -0500

    new vendor doc

--- src/Pages/Vendor.js
+++ src/Pages/Vendor.js
@@ -11,7 +11,7 @@ function Vendor() {
              <img className='divider' src={Dividerclear} alt="Divider" />
             <a href='https://www.facebook.com/events/737844167622989?acontext=%7B%22event_action_history%22%3A[%7B%22extra_data%22%3A%22%22%2C%22mechanism%22%3A%22search_results%22%2C%22surface%22%3A%22bookmark_search%22%7D%2C%7B%22extra_data%22%3A%22%22%2C%22mechanism%22%3A%22left_rail%22%2C%22surface%22%3A%22bookmark%22%7D%2C%7B%22extra_data%22%3A%22%22%2C%22mechanism%22%3A%22calendar_tab_event%22%2C%22surface%22%3A%22bookmark_calendar%22%7D]%2C%22ref_notif_type%22%3Anull%7D'><img src={Divine} alt="Divine" /></a>
             {/* The vendor registration form. */}
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe673PR_U_adG8zRbSBVfxGx2eyretoO9oN4xFM0kmxm5m_Mg/viewform?embedded=true" width="100%" height="2578" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc-bgdPiqlhQu0UGlEp4dm94IkD4urPl_1x-ySqbT6j0hKobA/viewform?embedded=true" width="100%" height="2578" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             <img className='divider' src={Dividerclear} alt="Divider" />
         </div >
     );
