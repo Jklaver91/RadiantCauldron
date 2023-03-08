f2152628d0e68e5d8f02cc6143a74f3fc7a29df9
Justin Klaver <Klaverts@yahoo.com>
2023-03-07 22:37:58 -0500

    updating events

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -15,8 +15,8 @@ function Home() {
             <img src={Logo} alt="Logo" />
             <img className='divider' src={Dividerclear} alt="Divider" />
             {/* Current Event */}
-            <h1 className='text my-3'>Join us at our next market!</h1>
-            <a href='/events'><img className='event' src={Divine} alt="Divine" /></a>
+            {/* <h1 className='text my-3'>Join us at our next market!</h1>
+            <a href='/events'><img className='event' src={Divine} alt="Divine" /></a> */}
             <img className='divider' src={Dividerclear} alt="Divider" />
             {/* Event Calender */}
             <div className="mx-3">
@@ -27,7 +27,7 @@ function Home() {
                             <h1>Featured Events</h1>
                             <img className='dividerclear' src={Dividerclear} alt="Divider" />
                         </div>
-                        <div className="col-lg-3 col-sm-12">
+                        {/* <div className="col-lg-3 col-sm-12">
                             <Card className="mx-auto my-3 fcard" style={{ width: '18rem' }}>
                                 <Card.Body>
                                     <Card.Link className="text-decoration-none eventlink" href="https://www.facebook.com/events/764949961766476/?acontext=%7B%22ref%22%3A%2252%22%2C%22action_history%22%3A%22[%7B%5C%22surface%5C%22%3A%5C%22share_link%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22share_link%5C%22%2C%5C%22extra_data%5C%22%3A%7B%5C%22invite_link_id%5C%22%3A1141564776485509%7D%7D]%22%7D">Roosterpalooza, The Rooster. Feb. 18th 11am-4pm</Card.Link>
@@ -43,7 +43,7 @@ function Home() {
                                     <Card.Link className="text-decoration-none eventlink" href="https://www.facebook.com/events/417892930551679/?acontext=%7B%22ref%22%3A%2252%22%2C%22action_history%22%3A%22[%7B%5C%22surface%5C%22%3A%5C%22share_link%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22share_link%5C%22%2C%5C%22extra_data%5C%22%3A%7B%5C%22invite_link_id%5C%22%3A1140683403257465%7D%7D]%22%7D">Springtime Blooms Ancient Elm, . Mar. 4th 10am-4pm</Card.Link>
                                 </Card.Body>
                             </Card>
-                        </div>
+                        </div> */}
                         <div className="col-lg-3 col-sm-12">
                             <Card className="mx-auto my-3 fcard" style={{ width: '18rem' }}>
                                 <Card.Body>
